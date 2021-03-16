3ef1bc2ff43a03b21b985678180eef12f5bcf427
dong huang <huangdgm@gmail.com>
2021-03-16 17:37:22 +0800

    Added a S3 bucket and a dynamodb table.

--- main.tf
+++ main.tf
@@ -1,3 +1,33 @@
 provider "aws" {
 	region = "us-east-2"
 }
+
+resource "aws_s3_bucket" "terraform_state" {
+	bucket = "terraform3-up-and-running"
+
+	lifecycle {
+		prevent_destroy = true
+	}
+
+	versioning {
+		enabled = true
+	}
+
+	server_side_encryption_configuration {
+		rule {
+			apply_server_side_encryption_by_default {
+				sse_algorithm = "AES256"
+			}
+		}
+	}
+}
+
+resource "aws_dynamodb_table" "terraform-locks" {
+	hash_key = "LockID"
+	name = "terraform-up-and-running-locks"
+	billing_mode = "PAY_PER_REQUEST"
+	attribute {
+		name = "LockID"
+		type = "S"
+	}
+}
